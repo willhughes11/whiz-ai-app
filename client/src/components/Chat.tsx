e90ce390daac9ce88a39e3c48e72a21f543606ae
willhughes11 <willhughes2110@gmail.com>
2023-12-22 22:21:22 -0500

    finally starting react native app

---
+++ client/src/components/Chat.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Chat = () => {
+  return (
+    <View className="flex-1">
+      <Text>Chat</Text>
+    </View>
+  );
+};
+
+export default Chat;
